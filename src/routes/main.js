fb5cea53aa90ce61d3c4957ec5e0a3e0f23cff71
hqro <gui.lariviere@gmail.com>
2016-11-22 12:59:11 +0100

    add test routes

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import { validateWebhook, analyseMessage } from '../controllers/webhook';
 
+// import { testCoord, testVelib } from '../controllers/location';
+
 const router = express.Router();
 
 router.get('/webhook', validateWebhook);
 router.post('/webhook', analyseMessage);
+
+// router.get('/coord/:origin/:destination', testCoord);
+// router.get('/velib/:address', testVelib);
 
 router.get('*', (req, res) => {
   res.send('It seems to work fine :)');
